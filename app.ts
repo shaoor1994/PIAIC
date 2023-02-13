cbf1a4ef329568adac37806c5633f50f7d5a6f07
shaoor1994 <saadshaoor94@gmail.com>
2023-02-13 19:25:18 +0500

    variables

--- app.ts
+++ app.ts
@@ -9,3 +9,21 @@ let f = 10.9;
 f = 22;
 let g = false;
 g = true;
+
+
+//use const where variable values do not change
+const aa =  5;
+const bb : number = 33;
+const cc ="best";
+
+//I suggest use let instead of var everywhere, 
+//becuase let has blocked scope
+if (true) {
+	let z = 4;
+	//use z
+}
+else {
+	let z = "string";
+	//use z
+}
+console.log("let: " + z);// Error: z is not defined in this scope
