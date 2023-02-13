fcebdde4c656845d7ef4fdc84cccc97d979c7304
shaoor1994 <saadshaoor94@gmail.com>
2023-02-13 19:31:38 +0500

    import-without-branch

--- app.ts
+++ app.ts
@@ -1,29 +1,4 @@
-let a : string = "Pakistan";
-a = "USA";
-let b : number = 9;
-let c : boolean = true;
+import a from "../src/first";
+import {b, c as d} from "../src/second";
 
-//type inference
-let e = "USA";
-let f = 10.9;
-f = 22;
-let g = false;
-g = true;
-
-
-//use const where variable values do not change
-const aa =  5;
-const bb : number = 33;
-const cc ="best";
-
-//I suggest use let instead of var everywhere, 
-//becuase let has blocked scope
-if (true) {
-	let z = 4;
-	//use z
-}
-else {
-	let z = "string";
-	//use z
-}
-console.log("let: " + z);// Error: z is not defined in this scope
+console.log(a + b + d);
