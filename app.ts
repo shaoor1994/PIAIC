1d85d5fecaeea5294ac1c45bca32b38a9004f6d6
shaoor1994 <saadshaoor94@gmail.com>
2023-02-15 21:50:46 +0500

    nested-types

--- app.ts
+++ app.ts
@@ -1,126 +1,145 @@
-// // import a from "./src/first.js";
-// // import {b, c as d} from "./src/second.js";
+// // // import a from "./src/first.js";
+// // // import {b, c as d} from "./src/second.js";
 
-// // console.log(a + b + d);
+// // // console.log(a + b + d);
 
-// // import inquirer from "inquirer";
-// // import chalk from "chalk";
+// // // import inquirer from "inquirer";
+// // // import chalk from "chalk";
 
-// // let answers = inquirer.prompt([{
-// //     name: "age",
-// //     type: "number",
-// //     message: "Enter your Age:"}
-// // ]);
+// // // let answers = inquirer.prompt([{
+// // //     name: "age",
+// // //     type: "number",
+// // //     message: "Enter your Age:"}
+// // // ]);
 
-// // console.log("Insha Allah, in " + (60 - answers.name) + " years you will be 60 years old.");
+// // // console.log("Insha Allah, in " + (60 - answers.name) + " years you will be 60 years old.");
 
-// let myname: string | null;
+// // let myname: string | null;
 
-// myname = null;
-// console.log(myname);
+// // myname = null;
+// // console.log(myname);
 
-// myname = "saad";
-// console.log(myname);
+// // myname = "saad";
+// // console.log(myname);
 
-// //myname = undefined; //Error
-// //myname = 12; //Error
+// // //myname = undefined; //Error
+// // //myname = 12; //Error
 
-// let myAge: string | number;
+// // let myAge: string | number;
 
-// myAge = 16;//narrowing
-// console.log(myAge);
+// // myAge = 16;//narrowing
+// // console.log(myAge);
 
-// //console.log(myAge.toLowerCase());//Error
+// // //console.log(myAge.toLowerCase());//Error
 
-// myAge = "Dont Know";//narrowing
-// console.log(myAge);
+// // myAge = "Dont Know";//narrowing
+// // console.log(myAge);
 
-// console.log(myAge.toString()); // common to both types 
-//                                //can be called even without narrowing
+// // console.log(myAge.toString()); // common to both types 
+// //                                //can be called even without narrowing
 
-// console.log(myAge.toLowerCase());//Can be called on string 
-//                                 //because of narrowing
+// // console.log(myAge.toLowerCase());//Can be called on string 
+// //                                 //because of narrowing
 
-// let newAge = Math.random() > 0.6 ? "Khan": 60;
+// // let newAge = Math.random() > 0.6 ? "Khan": 60;
 
-// //newAge.toLowerCase();//Error: Transpiler cannot narrow
+// // //newAge.toLowerCase();//Error: Transpiler cannot narrow
 
-// if (newAge === "Khan") {
-//     // Type of newAge: string
-//     newAge.toUpperCase(); // Can be called
+// // if (newAge === "Khan") {
+// //     // Type of newAge: string
+// //     newAge.toUpperCase(); // Can be called
+// // }
+
+// // if(typeof newAge === "string"){
+// //     // Type of newAge: string
+// //     newAge.toUpperCase(); // Can be called
+// // }
+
+// // typeof newAge === "string"
+// // ? newAge.toUpperCase() // Ok: string
+// // : newAge.toFixed(); // Ok: number
+
+
+// // let age: number | "died" | "unknown";
+
+// // age = 90;//OK
+// // age = "died";//OK
+// // age = "unknown";//OK
+// // //age = "living";//Error
+
+
+// // let saad: "saad";
+
+// // saad = "saad";
+// // //saad = "khan";//Error
+
+
+// // let yourName = Math.random() > 0.6 ? "Saad Shaoor Ghazanfar": undefined;
+
+// // if (yourName) {
+// //     yourName.toUpperCase(); // Ok: string
+// // }
+
+// // //yourName.toUpperCase();//Error: Object is possibly 'undefined'.
+
+// // yourName?.toUpperCase();//OK
+
+// // // You can also define a type alias
+// // type RawData = boolean | number | string | null | undefined;
+
+// // let data: RawData;
+
+// // // You can even combine them
+
+// // type Id = number | string;
+
+// // type IdMaybe = Id | undefined | null;
+
+// let teacher : {name: string, exp: number} = {
+//     name: "fahad",
+//     exp: 10
 // }
 
-// if(typeof newAge === "string"){
-//     // Type of newAge: string
-//     newAge.toUpperCase(); // Can be called
+// // Aliased Object Type
+// type Student = {
+//     name: string,
+//     age?: number
 // }
 
-// typeof newAge === "string"
-// ? newAge.toUpperCase() // Ok: string
-// : newAge.toFixed(); // Ok: number
-
-
-// let age: number | "died" | "unknown";
-
-// age = 90;//OK
-// age = "died";//OK
-// age = "unknown";//OK
-// //age = "living";//Error
-
-
-// let saad: "saad";
-
-// saad = "saad";
-// //saad = "khan";//Error
-
-
-// let yourName = Math.random() > 0.6 ? "Saad Shaoor Ghazanfar": undefined;
-
-// if (yourName) {
-//     yourName.toUpperCase(); // Ok: string
+// let student: Student = {
+//     name: "saad",
+//     age: 30
 // }
 
-// //yourName.toUpperCase();//Error: Object is possibly 'undefined'.
+// console.log(student["name"]);
+// console.log(student.age);
 
-// yourName?.toUpperCase();//OK
+// // Interfaces
 
-// // You can also define a type alias
-// type RawData = boolean | number | string | null | undefined;
+// interface Manager {
+//     name: string,
+//     subordiates?: number
+// }
 
-// let data: RawData;
+// let storeManager: Manager = {
+//     name: "hammad"
+// }
 
-// // You can even combine them
 
-// type Id = number | string;
+type Author = {
+    firstName: string;
+    lastName: string;
+};
 
-// type IdMaybe = Id | undefined | null;
+type Book = {
+    author: Author;
+    name: string;
+};
 
-let teacher : {name: string, exp: number} = {
-    name: "fahad",
-    exp: 10
-}
-
-// Aliased Object Type
-type Student = {
-    name: string,
-    age?: number
-}
-
-let student: Student = {
-    name: "saad",
-    age: 30
-}
-
-console.log(student["name"]);
-console.log(student.age);
-
-// Interfaces
-
-interface Manager {
-    name: string,
-    subordiates?: number
-}
-
-let storeManager: Manager = {
-    name: "hammad"
+const myBook : Book = {
+    author: {
+        firstName: "Saad",
+        lastName: "Shaoor"
+    },
+    name: "My Best Book"
 }
