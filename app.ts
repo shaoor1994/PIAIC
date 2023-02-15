d6ae8d8cf45c38af2eafd228ef7c3581736f40d5
shaoor1994 <saadshaoor94@gmail.com>
2023-02-15 21:59:29 +0500

    intersection-type

--- app.ts
+++ app.ts
@@ -126,20 +126,41 @@
 // }
 
 
-type Author = {
-    firstName: string;
-    lastName: string;
-};
+// type Author = {
+//     firstName: string;
+//     lastName: string;
+// };
 
-type Book = {
-    author: Author;
+// type Book = {
+//     author: Author;
+//     name: string;
+// };
+
+// const myBook : Book = {
+//     author: {
+//         firstName: "Saad",
+//         lastName: "Shaoor"
+//     },
+//     name: "My Best Book"
+// }
+interface Student {
+    student_id: number;
     name: string;
-};
-
-const myBook : Book = {
-    author: {
-        firstName: "Saad",
-        lastName: "Shaoor"
-    },
-    name: "My Best Book"
+  }
+    
+interface Teacher {
+    teacher_Id: number;
+    teacher_name: string;
 }
+    
+type intersected_type = Student & Teacher;
+    
+let obj1: intersected_type = {
+    student_id: 3232,
+    name: "rita",
+    teacher_Id: 7873,
+    teacher_name: "seema",
+};
+    
+console.log(obj1.teacher_Id);
+console.log(obj1.name);
