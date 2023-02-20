33c557933f68c24ba43a1cec01e951b3da94c242
shaoor1994 <saadshaoor94@gmail.com>
2023-02-20 23:01:10 +0500

    enums

--- src/intial.ts
+++ src/intial.ts
@@ -322,3 +322,74 @@
     
 // console.log(obj1.teacher_Id);
 // console.log(obj1.name);
+
+// // Any
+
+// let myval: any;
+
+// myval = true; // OK
+// myval = 42; // OK
+// myval = "hey!"; // OK
+// myval = []; // OK
+// myval = {}; // OK
+// myval = Math.random; // OK
+// myval = null; // OK
+// myval = undefined; // OK
+// myval = () => { console.log('Hey again!'); }; // OK
+
+
+// //Unknown
+
+// let value: unknown;
+
+// value = true; // OK
+// value = 42; // OK
+// value = "hey!"; // OK
+// value = []; // OK
+// value = {}; // OK
+// value = Math.random; // OK
+// value = null; // OK
+// value = undefined; // OK
+// value = () => { console.log('Hey again!'); }; // OK
+
+
+// // Assigning a value of type unknown to variables of other types
+
+// let val: unknown;
+
+// const val1: unknown = val; // OK
+// const val2: any = val; // OK
+// // const val3: boolean = val; // Will throw error
+// // const val4: number = val; // Will throw error
+// // const val5: string = val; // Will throw error
+// // const val6: Record<string, any> = val; // Will throw error
+// // const val7: any[] = val; // Will throw error
+// // const val8: (...args: any[]) => void = val; // Will throw error
+
+
+// // Never
+
+// // Function returning never must not have a reachable end point
+// function error(message: string): never {
+//   throw new Error(message);
+// }
+ 
+// // Inferred return type is never
+// function fail() {
+//   return error("Something failed");
+// }
+ 
+// // Function returning never must not have a reachable end point
+// function infiniteLoop(): never {
+//   while (true) {}
+// }
+// enum Color {Red, Green, Blue};
+// var c: Color = Color.Green;
+
+// enum Color1 {Red = 1, Green, Blue};
+// var colorName: string = Color1[2];
+// console.log(colorName);
+
+// enum Color2 {Red = 1, Green = 2, Blue = 4};
+// var colorIndex = Color2["Blue"];
+// console.log(colorIndex);
