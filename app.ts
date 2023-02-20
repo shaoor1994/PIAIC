6d8c72408dc227c7f3b047417f137a5c9aae29fa
shaoor1994 <saadshaoor94@gmail.com>
2023-02-20 23:05:57 +0500

    arrays

--- app.ts
+++ app.ts
@@ -1,10 +1,23 @@
-const enum Color {Red, Green, Blue};
-var c: Color = Color.Green;
+// const enum Color {Red, Green, Blue};
+// var c: Color = Color.Green;
 
-const enum Color1 {Red = 1, Green, Blue};
-var colorName: string = Color[2]; 
-console.log(colorName);
 
-const enum Color2 {Red = 1, Green = 2, Blue = 4};
-var colorIndex = Color2["Blue"];
-console.log(colorIndex);
+// const enum Color1 {Red = 1, Green, Blue};
+// var colorName: string = Color[2]; 
+// console.log(colorName);
+
+// const enum Color2 {Red = 1, Green = 2, Blue = 4};
+// var colorIndex = Color2["Blue"];
+// console.log(colorIndex);
+
+// Arrays
+
+// let array1 : number[] = [5, 6, 7];
+// console.log(array1[1]);
+// let array2:Array<number> = [1, 2, 3];
+// let array3 : number[] = [];
+
+// let array4: number[] = new number[2];
+
+// let array5: number[] = [];
+// array5.push(1234);
