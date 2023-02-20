61dc075144dc54edadbb3531acce911700900993
shaoor1994 <saadshaoor94@gmail.com>
2023-02-20 23:18:26 +0500

    rest

--- app.ts
+++ app.ts
@@ -95,4 +95,17 @@
 //       return firstName;
 // }
 
+function buildName(firstName: string, ...restOfName: string[]) {//Named function with Rest parameters
+	return firstName + " " + restOfName.join(" ");
+}
 
+var employeeName = buildName("Joseph", "Samuel", "Lucas", "MacKinzie");
+
+
+
+
+//anonymous function type with Rest parameters
+var buildNameFun: (fname: string, ...rest: string[])=>string =
+function (firstName: string, ...restOfName: string[]) {
+	return firstName + " " + restOfName.join(" ");
+}
