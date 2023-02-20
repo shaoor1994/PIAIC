d5fc2e8e074f22cbfa877f6218e154efd2ab9ef5
shaoor1994 <saadshaoor94@gmail.com>
2023-02-20 23:19:58 +0500

    callback

--- app.ts
+++ app.ts
@@ -95,17 +95,28 @@
 //       return firstName;
 // }
 
-function buildName(firstName: string, ...restOfName: string[]) {//Named function with Rest parameters
-	return firstName + " " + restOfName.join(" ");
+// function buildName(firstName: string, ...restOfName: string[]) {//Named function with Rest parameters
+// 	return firstName + " " + restOfName.join(" ");
+// }
+
+// var employeeName = buildName("Joseph", "Samuel", "Lucas", "MacKinzie");
+
+
+
+
+// //anonymous function type with Rest parameters
+// var buildNameFun: (fname: string, ...rest: string[])=>string =
+// function (firstName: string, ...restOfName: string[]) {
+// 	return firstName + " " + restOfName.join(" ");
+// }
+
+function myCallBack(text: string) {
+  console.log("inside myCallback " + text);
 }
 
-var employeeName = buildName("Joseph", "Samuel", "Lucas", "MacKinzie");
-
-
-
-
-//anonymous function type with Rest parameters
-var buildNameFun: (fname: string, ...rest: string[])=>string =
-function (firstName: string, ...restOfName: string[]) {
-	return firstName + " " + restOfName.join(" ");
+function callingFunction(initialText: string, callback: (text: string) => void)
+{
+  callback(initialText);
 }
+
+callingFunction("myText", myCallBack);
