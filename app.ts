ff25000d30a95372987ef805b54169fe76665b88
shaoor1994 <saadshaoor94@gmail.com>
2023-02-20 23:22:27 +0500

    overload

--- app.ts
+++ app.ts
@@ -120,17 +120,19 @@
 // }
 
 // callingFunction("myText", myCallBack);
-function add(arg1: string, arg2: string): string;//option 1
-function add(arg1: number, arg2: number): number;//option 2
-function add(arg1: boolean, arg2: boolean): boolean;//option 3
-//this is not part of the overload list, 
-//so it has only three overloads
-function add(arg1: any, arg2: any): any {
-    return arg1 + arg2;
-}
+// function add(arg1: string, arg2: string): string;//option 1
+// function add(arg1: number, arg2: number): number;//option 2
+// function add(arg1: boolean, arg2: boolean): boolean;//option 3
+// //this is not part of the overload list, 
+// //so it has only three overloads
+// function add(arg1: any, arg2: any): any {
+//     return arg1 + arg2;
+// }
 
-//Calling 'add' with any other parameter types would cause an error except for the three options
-console.log(add(1, 2));
-console.log(add("Hello", "World"));
-console.log(add(true, false));
+// //Calling 'add' with any other parameter types would cause an error except for the three options
+// console.log(add(1, 2));
+// console.log(add("Hello", "World"));
+// console.log(add(true, false));
+
+
 
